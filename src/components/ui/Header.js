b932c00e2d5a913d57b0c5a14182d6ad77269b37
Rohit Girotra <rohitgirotra555@gmail.com>
2021-01-17 17:38:07 +0530

    custom-arrow added

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -4,8 +4,20 @@ import logo from '../../img/logo.png'
 const Header = () => {
   return (
     <header className='center'>
+    <div>
       <img src={logo} alt='' />
+    </div>
+    <div className='custom-select'>
+      <select>
+        <option value="">RCB</option>
+        <option value="">CSK</option>
+      </select>
+      <span className='custom-arrow'></span>
+    </div>
+   
+    
     </header>
+    
   )
 }
 
